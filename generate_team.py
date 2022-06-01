0a3d9f29c443374446d03127e49e03d8b3feb312
Benjamin Barthe <benjamin.barthe@atos.net>
2022-06-01 13:49:59 +0200

    first commit

---
+++ generate_team.py
@@ -0,0 +1,14 @@
+
+import random
+
+members = [ "Samuel", "Thomas", "Thibault", "Cedric", "Gael", "Benjamin", "Nicolas LDP", "Nicolas Labro", "Dieter", "Lucas" ] 
+
+team = []
+i=1
+while len(members) > 0:
+	team.append(members.pop(random.randrange(len(members))))
+	if len(team) == 2:
+		print("Team ", i, str(team))
+		team = []
+		i+=1
+
